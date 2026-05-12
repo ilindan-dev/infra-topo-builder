58cdab8eedb7041c3094f5fc6ccc51c737ca63e5
Gellert Ilya <gellertilya.job@gmail.com>
2026-05-12 03:27:14 +0300

    feat: implement FSM log parser, REST handlers and orchestration service

--- builder/cmd/builder/main.go
+++ builder/cmd/builder/main.go
@@ -1,1 +1,27 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("not implemented")
+}
+
+// func run(cfg config.Config, log *slog.Logger) error {
+//	return nil
+// }
+
+// func mustMakeLogger(logLevel string) *slog.Logger {
+//	var level slog.Level
+//	switch logLevel {
+//	case "DEBUG":
+//		level = slog.LevelDebug
+//	case "INFO":
+//		level = slog.LevelInfo
+//	case "ERROR":
+//		level = slog.LevelError
+//	default:
+//		panic("unknown log level: " + logLevel)
+//	}
+//	handler := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: level})
+//	return slog.New(handler)
+// }
---
+++ tests/go.mod
@@ -0,0 +1,3 @@
+module github.com/ilindan-dev/infra-topo-builder/tests
+
+go 1.26.2
