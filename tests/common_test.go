81ba82dbcf8e3e626a897fd3a62e72f8bef0f27d
Gellert Ilya <gellertilya.job@gmail.com>
2026-05-15 14:33:53 +0300

    test(parser): finalize unit tests and cleanup integration tests directory

--- tests/common_test.go
+++ tests/common_test.go
@@ -1,1 +1,35 @@
 package tests
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+const address = "http://localhost:8080"
+
+var client = http.Client{
+	Timeout: 10 * time.Minute,
+}
+
+func TestPreflight(t *testing.T) {
+	require.Equal(t, true, true)
+}
+
+type PingResponse struct {
+	Status string `json:"status"`
+}
+
+func TestPing(t *testing.T) {
+	resp, err := client.Get(address + "/ping")
+	require.NoError(t, err, "cannot ping")
+	defer resp.Body.Close()
+	require.Equal(t, http.StatusOK, resp.StatusCode, "wrong status")
+
+	var reply PingResponse
+	require.NoError(t, json.NewDecoder(resp.Body).Decode(&reply))
+	require.Equal(t, "ok", reply.Status, "no words running")
+}
